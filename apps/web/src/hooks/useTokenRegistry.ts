10085da35257e681e5502daf486daca7cc318eb8
DENIS <seito@ukr.net>
2026-03-15 10:52:05 +0200

    Create useTokenRegistry.ts

---
+++ apps/web/src/hooks/useTokenRegistry.ts
@@ -0,0 +1,29 @@
+"use client";
+
+import { useReadContract, useWriteContract } from "wagmi";
+import {
+  STUDENT_TOKEN_REGISTRY_ADDRESS,
+  studentTokenRegistryAbi,
+} from "../lib/contracts";
+
+export function useTokenRegistry() {
+  const allTokensQuery = useReadContract({
+    address: STUDENT_TOKEN_REGISTRY_ADDRESS,
+    abi: studentTokenRegistryAbi,
+    functionName: "getAllTokens",
+    query: {
+      enabled:
+        !!STUDENT_TOKEN_REGISTRY_ADDRESS &&
+        STUDENT_TOKEN_REGISTRY_ADDRESS !==
+          "0x0000000000000000000000000000000000000000",
+    },
+  });
+
+  const { writeContractAsync, isPending } = useWriteContract();
+
+  return {
+    allTokensQuery,
+    registerToken: writeContractAsync,
+    isRegisterPending: isPending,
+  };
+}
